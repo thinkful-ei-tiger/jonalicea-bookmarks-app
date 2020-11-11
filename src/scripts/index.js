634510dd84d7d20877af5f394ecde02eb87fcbab
Jon Alicea <me@jhalicea.com>
2020-11-11 12:40:22 -0500

    Revert "Initial dist subtree commit"

---
+++ src/scripts/index.js
@@ -0,0 +1,177 @@
+import 'normalize.css';
+import '../css/index.css';
+import $ from 'jquery';
+
+import database from './database';
+import templates from './templates';
+
+
+
+
+const render = function () {
+  const bookmarks = database.STORE.bookmarks;
+  console.log('render');
+
+  if(database.STORE.adding === true) {
+    const addView = templates.addNewBookmarkTemplateView();
+    $('#all-bookmarks').html(addView);
+  } else if (database.STORE.editing === true) {
+    const addEdit = templates.generateEditBookmarktemplate();
+    $('#all-bookmarks').html(addEdit);
+  } else if(database.STORE.filter > 0 ) {
+    const filtered = filteredBookmarks(bookmarks);
+    const bookmarksHTMLString = templates.generateBookmarkHTMLstring(filtered);
+    $('#all-bookmarks').html(bookmarksHTMLString);
+  } else {
+    //render the bookmarks to initial view
+    const bookmarksHTMLString = templates.generateBookmarkHTMLstring(bookmarks);
+    $('#all-bookmarks').html(bookmarksHTMLString);
+  }
+
+};
+
+const addBookmarkEventListener = function () {
+  $('#add-bookmark').on('click',  function () {
+    database.STORE.adding = true;
+    render();
+  });
+};
+
+const filteredBookmarks = function (bookmarks) {
+  let filter = database.STORE.filter;
+  let filteredBookmarks = [];
+  bookmarks.forEach(function (bookmark){
+    if (filter <= bookmark.rating) {
+      filteredBookmarks.push(bookmark);
+    }
+    
+  });
+  return filteredBookmarks;
+};
+
+
+const expandedBookmark = function (id) {
+  const foundBookmark = database.STORE.bookmarks.find(bookmark => bookmark.id === id);
+  foundBookmark.expanded = !foundBookmark.expanded;
+};
+
+const getBookmarkIdFromElement = function (element) {
+  return $(element)
+    .closest('.bookmark')
+    .data('bookmark');
+};
+
+const expandBookmarksEventListener = function () {
+  $('#all-bookmarks').on('click', '.app-link',  function (event) {
+    const id = getBookmarkIdFromElement(event.currentTarget);
+    expandedBookmark(id);
+    render();
+  });
+};
+
+
+
+function serializeJson(form) {
+  const formData = new FormData(form);
+  const o = {};
+  formData.forEach((val, name) => o[name] = val);
+  return JSON.stringify(o);
+}
+
+const submitNewBookmarkEventListener = function () {
+  $('main').on('submit', '.add-new-bookmark', function (event){
+    event.preventDefault();
+    const formElemnet = $('.add-new-bookmark')[0];
+    const jsonData = serializeJson(formElemnet);
+    database.createBookmark(jsonData)
+      .then(response => {
+          return response.json();
+      })
+      .then((newBookmark) => {
+        if (newBookmark.message) {
+          $('.error').html(
+            `Something went wrong:
+            "Enter Title" and  "Enter url" Required. Please Use http:// or https:// on "Enter Url"`
+            );
+            console.log(newBookmark.message)
+        } else {
+                  database.addBookmarks(newBookmark); 
+        render();
+        }
+
+      })
+      .catch(err => {
+        console.log(err);
+        $('.error').html(`Something went wrong: ${err.message}.`);
+      
+      });
+    
+    // debugger;
+    // console.log(jsonData);
+    // const localData = JSON.parse(jsonData);
+    // database.STORE.bookmarks.push(localData);
+    // console.log(database.STORE.bookmarks);
+    database.STORE.adding = false;
+  });
+};
+
+const filterBookmarkEventListener = function () {
+  $('#filter-by-raiting').on('click', function (event){
+    event.preventDefault();
+    let filterValue = $('#filter-by-raiting').val();
+    database.STORE.filter = filterValue;
+    console.log(database.STORE.filter);
+    render();
+  });
+};
+
+
+
+const cancelNewBookmarkEventListener = function (){
+  $('main').on('click', '#cancel-bookmark', function (event){
+    event.preventDefault();
+    database.STORE.adding = false;
+    render();
+  });
+};
+
+const deleteBookmarkEventListener = function() {
+  $('main').on('click', '.delete-bookmark', function (event){
+    console.log('delete');
+    event.preventDefault();
+    const id = getBookmarkIdFromElement(event.currentTarget);
+    database.deleteBookmark(id)
+      .then(response => response.json())
+      .then(() => {
+        database.storeDeleteBookmark(id);
+        render();
+      });
+      
+  });
+};
+
+
+const eventListeners = function () {
+  addBookmarkEventListener();
+  filterBookmarkEventListener();
+  expandBookmarksEventListener();
+  submitNewBookmarkEventListener();
+  cancelNewBookmarkEventListener();
+  deleteBookmarkEventListener();
+  
+
+};
+
+const app = function (){
+
+  database.getBookmarks()
+    .then(response => response.json())
+    .then((bookmarks) => {
+      bookmarks.forEach((bookmark) => database.addBookmarks(bookmark));
+      render();
+    });
+  eventListeners();
+
+};
+
+$(app);
---
+++ src/scripts/templates.js
@@ -0,0 +1,106 @@
+
+const initialBookmarkTemplateView = function (bookmark) {
+  let bookmarkTitle = `<a href="#" class="app-link"><h2><span>${bookmark.title}</span></h2></a>`;
+  let totalRating = '';
+  let i = 0;
+  while (bookmark.rating > i) {
+    totalRating += '⭐';
+    i++;
+  }
+
+  let bookmarkRaiting = `<div class="bookmark-raiting">${totalRating}</div>`;
+  let bookmarkDescription = `<p class="bookmark-description">${bookmark.desc}</p>`;
+  let bookmarkExpanded = '';
+  let bookmarkAdd = '';
+  //look at the state of the bookmark list
+  if(bookmark.expanded === true) {
+    bookmarkExpanded = `<div class="expanded-controls">
+                                <a class="visit-website" href="${bookmark.url}" target="_blank">Open 🔗</a>
+                                <button class="delete-bookmark">Delete</button>
+                            </div>
+                                ${bookmarkDescription}
+                            <!--<div class="edit">
+                             <button class="edit-bookmark">Edit</button> 
+                            </div>-->`;
+  }
+    
+  return `<div class="bookmark" data-bookmark="${bookmark.id}">
+                ${bookmarkTitle}
+                ${bookmarkAdd}
+                ${bookmarkExpanded}
+                ${bookmarkRaiting}
+             </div>`;
+};
+
+const generateBookmarkHTMLstring = function (bookmarks) {
+  const bookmarksString = bookmarks.map((bookmark) => initialBookmarkTemplateView(bookmark));
+  return bookmarksString.join('');
+};
+
+
+const addNewBookmarkTemplateView = function (){
+
+  //you have to find the one to edit by using id
+  let titleH2 = 'Create Bookmark';
+  const bookmarkAdd = `<h2>${titleH2}</h2>
+                          <form class="add-new-bookmark">
+                              <div class="error"></div>
+                              <label class="hide-label" for="title">Enter Title</label>
+                              <!-- <input type="text" title="Enter Title" name="title" id="title" placeholder="Enter Title" pattern="[a-zA-Z0-9_]+.*$" minlength="1" required> -->
+                              <input type="text" title="Enter Title" name="title" id="title" placeholder="Enter Title">
+                              <label class="hide-label" for="url">Enter URL</label>
+                              <!-- <input type="text" title="Enter URL" name="url" value="https://" id="url" placeholder="https://website.com" pattern="https://.*|http://.*"  minlength="5" required> -->
+                              <input type="text" title="Enter URL" name="url" value="https://" id="url" placeholder="https://website.com">
+                              <label class="hide-label" for="desc">Enter Description</label>
+                              <textarea title="Enter Description" name="desc" id="desc" placeholder="Enter Description"></textarea>
+
+                              <div class="add-raiting">
+                                  <fieldset class="rating">
+                                  <legend>Enter Rating</legend>
+                                      <input type="radio" id="star5" name="rating" value="5" /><label for="star5">5 Stars</label>
+                                      <input type="radio" id="star4" name="rating" value="4" /><label for="star4">4 Stars</label>
+                                      <input type="radio" id="star3" name="rating" value="3" /><label for="star3">3 Stars</label>
+                                      <input type="radio" id="star2" name="rating" value="2" /><label for="star2">2 Stars</label>
+                                      <input type="radio" id="star1" name="rating" value="1" /><label for="star1">1 Star</label>
+                                  </fieldset>
+                              
+                              </div>
+                                  <button id="save-bookmark">Save 🔖</button>
+                                  <button id="cancel-bookmark">Cancel</button>
+                              </div>
+                          </form>`;
+  return bookmarkAdd;
+};
+  
+const editBookmarkTemplateView = function (){
+  let titleH2 = 'Edit Bookmark';
+  let title = db.bookmarks.title;
+  
+  const bookmarkEdit = `<h2>${titleH2}</h2>
+                          <form class="add-new-bookmark">
+                              <input type="text" name="title" id="title" placeholder="Title" value="${title}" required>
+                              <input type="text" name="url" id="url" placeholder="https://website.com" required>
+                              <textarea name="description" id="description" placeholder="Description"></textarea>
+                              <div class="add-raiting">
+                                  <fieldset class="rating">
+                                      <input type="radio" id="star5" name="rating" value="5" /><label for="star5">5 Stars</label>
+                                      <input type="radio" id="star4" name="rating" value="4" /><label for="star4">4 Stars</label>
+                                      <input type="radio" id="star3" name="rating" value="3" /><label for="star3">3 Stars</label>
+                                      <input type="radio" id="star2" name="rating" value="2" /><label for="star2">2 Stars</label>
+                                      <input type="radio" id="star1" name="rating" value="1" /><label for="star1">1 Star</label>
+                                  </fieldset>
+                              </div>
+                                  <button type="submit" id="save-edited-bookmark">Save 🔖</button>
+                                  <button id="cancel-bookmark">Cancel</button>
+                              </div>
+                          </form>`;
+  return bookmarkEdit;
+  
+};
+
+export default {
+  initialBookmarkTemplateView,
+  generateBookmarkHTMLstring,
+  addNewBookmarkTemplateView,
+  editBookmarkTemplateView
+};
